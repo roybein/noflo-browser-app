c94c8ac32475ea53fabbeb9175a2dcd2ef37f349
Roybein B <roybein@gmail.com>
2022-09-16 20:17:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# noflo-browser-app
+
+[Hello World](http://noflojs.org/noflo-browser-app/)
+
+Usage
+-------
+
+* Fork this project on GitHub
+* Import in Flowhub
+* Make changes, syncronize in Flowhub
+* Add any [NoFlo modules](https://npmjs.com/browse/keyword/noflo) you need to `package.json`
+* Run local development server with `npm start`
+* Run tests with `npm test`
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './app.js',
+    test: './node_modules/noflo-runtime-headless/spec/build/webpack.entry.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /noflo([\\]+|\/)lib([\\]+|\/)loader([\\]+|\/)register.js$/,
+        use: [
+          {
+            loader: 'noflo-component-loader',
+            options: {
+              graph: null,
+              debug: true,
+              baseDir: __dirname,
+              manifest: {
+                runtimes: ['noflo'],
+                discover: true,
+              },
+              runtimes: [
+                'noflo',
+                'noflo-browser',
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s$/,
+        // exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.coffee$/,
+        use: [
+          {
+            loader: 'coffee-loader',
+            options: {
+              transpile: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.fbp$/,
+        use: [
+          {
+            loader: 'fbp-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'assets/*.html',
+          to: '[name].html',
+        },
+        {
+          from: 'assets/*.css',
+          to: '[name].css',
+        },
+      ],
+    }),
+    new webpack.ProvidePlugin({
+      process: ['process'],
+    }),
+  ],
+  resolve: {
+    extensions: ['.coffee', '.js'],
+    fallback: {
+      assert: false,
+      constants: false,
+      fs: false,
+      os: false,
+      path: require.resolve('path-browserify'),
+      util: require.resolve('util/'),
+    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    host: process.env.HOST || 'localhost',
+    port: 8080,
+    inline: true,
+  },
+};
